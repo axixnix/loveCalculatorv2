8c41a060372528b4b90dd3c1c287f1ff5048d3f8
B <henryirons@yahoo.com>
2021-05-25 15:44:31 +0100

    first commit

---
+++ app/Http/Controllers/LoveCalculatorController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class LoveCalculatorController extends Controller
+{
+    //
+    public function result(Request $request){
+
+        $firstName = $request->input('firstName');
+        $secondName = $request->input('secondName');
+        $name1 = str_split($firstName);
+        $name2 = str_split($secondName);
+        $firstCount = count($name1);
+        $secondCount = count($name2);
+        $intersectionCount =count(array_intersect($name1,$name2));
+        $ans =(($intersectionCount * 2)/($firstCount + $secondCount)) * 100;
+        
+
+        return response()->json('binary oracle says '.$firstName.' '.'and'.' '.$secondName.' '.'are'.' '.number_format($ans,2).'%'.' '.'compatible');
+
+    }
+}
